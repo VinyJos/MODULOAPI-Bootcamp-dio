038e3c0cbf848ac671221ade424d32b7bc83f75d
Vinicius Jose <viniciuswk@gmail.com>
2022-12-20 12:44:32 -0300

    criação do método GET, e PUT

--- Controllers/ContatoController.cs
+++ Controllers/ContatoController.cs
@@ -17,12 +17,49 @@ namespace MODULOAPI.Controllers
             _context = context;
         }
 
+        // Criar novo Contato
         [HttpPost] // -> estamos enviando uma informação
         public IActionResult Create(Contato contato)
         {
             _context.Add(contato);
             _context.SaveChanges();
             return Ok(contato);
+        }
+
+        // Buscar Contato salvo
+        [HttpGet("{id}")]
+        public IActionResult ObterPorId(int id)
+        {
+            var contato = _context.Contatos.Find(id);
+
+            // validação
+            if(contato == null)
+            {
+                return NotFound();
+            }
+            return Ok(contato);
+        }
+        
+        // Atualizar contato salvo
+        [HttpPut("{id}")]
+        public IActionResult Atualizar(int id, Contato contato)
+        {   
+            // vai buscar no banco
+            var contatoBanco = _context.Contatos.Find(id);
+
+            if(contatoBanco == null)
+                return NotFound();
+            
+            // As novas mudanças
+            contatoBanco.Nome = contato.Nome;
+            contatoBanco.Telefone = contato.Telefone;
+            contatoBanco.Ativo = contato.Ativo;
+
+            // vai fazer o update e salvar no banco
+            _context.Contatos.Update(contatoBanco);
+            _context.SaveChanges();
+
+            return Ok(contatoBanco);
         }
     }
 }
